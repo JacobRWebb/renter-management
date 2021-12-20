8bcba059ed42d03c545a5d20907529fdb36dae85
JacobRWebb <xxpwnage37xx@gmail.com>
2021-12-19 18:13:23 -0600

    Login Function

--- server/src/controllers/v1Controllers/userController.ts
+++ server/src/controllers/v1Controllers/userController.ts
@@ -1,3 +1,41 @@
-const userController = {};
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const prisma = new PrismaClient();
+
+const userController = {
+  login: async (username: string, password: string) => {
+    const user = await prisma.user.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      throw new Error("Password is incorrect");
+    }
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    return {
+      token,
+    };
+  },
+};
 
 export default userController;
--- server/src/routes/v1/userRoutes.ts
+++ server/src/routes/v1/userRoutes.ts
@@ -1,6 +1,23 @@
 import { Router } from "express";
+import { v1Controllers } from "../../controllers";
 
 const router = Router();
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const response = await v1Controllers.userController.login(
+      username,
+      password
+    );
+    res.cookie("token", response.token).json({ success: true });
+  } catch (e) {
+    res.json({
+      success: false,
+      error: e.message,
+    });
+  }
+});
 
 router.get("/", (_req, res) => {
   res.send("User Endpoint");
