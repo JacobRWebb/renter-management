c54f5dd3acb8efd185e41425f21da3035642cfe1
JacobRWebb <xxpwnage37xx@gmail.com>
2022-01-08 22:06:19 -0600

    Updated seedusers

--- server/src/util/seedUser.ts
+++ server/src/util/seedUser.ts
@@ -1,39 +1,21 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
-import { dropbox, stripe } from "./constants";
-const prisma = new PrismaClient();
+import { registerUser } from "../controllers/userController";
 
 export const seedUser = async () => {
-  const user = await prisma.user.findFirst({
-    where: { email: "test@gmail.com" },
-  });
+  let email = "admin@xodius.io";
+  let name = {
+    firstName: "Jacob",
+    middleInitial: "R",
+    lastName: "Webb",
+  };
+  let password = "password";
+  await registerUser(email, name, password, ["TENANT", "MANAGER", "ADMIN"]);
 
-  if (!user) {
-    const stripeCustomer = await stripe.customers.create({
-      email: "test@gmail.com",
-      name: "Test T User",
-    });
-
-    const newUser = await prisma.user.create({
-      data: {
-        email: "test@gmail.com",
-        password: await bcrypt.hash("password", 12),
-        stripeId: stripeCustomer.id,
-        name: {
-          create: {
-            firstName: "Test",
-            middleInitial: "T",
-            lastName: "User",
-          },
-        },
-      },
-    });
-
-    try {
-      await dropbox.filesCopyV2({
-        from_path: "/avatars/avatar.png",
-        to_path: `/avatars/${newUser.id}.png`,
-      });
-    } catch (error) {}
-  }
+  email = "test@gmail.com";
+  name = {
+    firstName: "Test",
+    middleInitial: "T",
+    lastName: "User",
+  };
+  password = "password";
+  await registerUser(email, name, password);
 };
